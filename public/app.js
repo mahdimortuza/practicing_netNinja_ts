211a4bb1ca57775f1e6b5b1f7304c49e00f20cc0
mahdimortuza <mahdi.webx@gmail.com>
2023-10-21 12:05:45 +0600

    dom manipulaging

--- public/app.js
+++ public/app.js
@@ -1,6 +1,10 @@
 "use strict";
-const ancor = document.querySelector('a');
-// if(ancor){
-// console.log(ancor.href);
-// }
-console.log(ancor.href);
+const form = document.querySelector('.new-item-form');
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
