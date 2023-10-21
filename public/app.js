f9e90003ecf19ea1d7509d420f22093435d37e10
mahdimortuza <mahdi.webx@gmail.com>
2023-10-21 17:12:53 +0600

    class interface

--- public/app.js
+++ public/app.js
@@ -6,6 +6,7 @@ docTwo = new Invoice('mario', 'plumbing work', 300);
 let docs = [];
 docs.push(docOne);
 docs.push(docTwo);
+console.log(docs);
 const invOne = new Invoice('mario', 'work on mario website', 250);
 const invTwo = new Invoice('luigi', 'work on luigi website', 300);
 const invoices = [];
--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,8 @@ let docs: HasFormatter[] = []
 docs.push(docOne)
 docs.push(docTwo)
 
+console.log(docs);
+
 const invOne = new Invoice('mario', 'work on mario website', 250)
 const invTwo = new Invoice('luigi', 'work on luigi website', 300)
 
@@ -22,3 +24,4 @@ invoices.push(invTwo)
 invoices.forEach(inv => {
     console.log(inv.client, inv.format());
 })
+ 
