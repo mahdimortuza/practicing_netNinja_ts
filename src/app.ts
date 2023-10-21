f550bbcb91429324b4610be2a4ecce8e9662232d
mahdimortuza <mahdi.webx@gmail.com>
2023-10-21 12:41:26 +0600

    access modifier

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,9 @@
 class Invoice {
-    readonly client: string
-    private details: string
-    public amount: number
-
-    constructor(c: string, d: string, a: number) {
-        this.client = c
-        this.details = d
-        this.amount = a
-    }
+    constructor(
+        readonly client: string,
+        public details: string,
+        private amount : number,
+    ) { }
 
     format() {
         return `${this.client} owes $${this.amount} for ${this.details}`
@@ -22,5 +18,5 @@ invoices.push(invOne)
 invoices.push(invTwo)
 
 invoices.forEach(inv => {
-    console.log(inv.client,  inv.amount, inv.format());
+    console.log(inv.client, inv.amount, inv.format());
 })
