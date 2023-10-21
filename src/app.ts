18b695dc7262f4c6f78baa51f937b37560f8aa2a
mahdimortuza <mahdi.webx@gmail.com>
2023-10-21 12:25:02 +0600

    exploring classes in ts

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,25 @@
-const form = document.querySelector('.new-item-form') as HTMLFormElement
+class Invoice {
+    client: string
+    details: string
+    amount: number
 
-const type = document.querySelector('#type') as HTMLSelectElement
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement
-const details = document.querySelector('#details') as HTMLInputElement
-const amount = document.querySelector('#amount') as HTMLInputElement
+    constructor(c: string, d: string, a: number) {
+        this.client = c
+        this.details = d
+        this.amount = a
+    }
 
-form.addEventListener('submit', (e: Event) => {
-    e.preventDefault()
-    console.log(
-        type.value,
-        tofrom.value,
-        details.value,
-        amount.valueAsNumber
-        );
-})
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`
+    }
+}
+
+const invOne = new Invoice('mario', 'work on mario website', 250)
+const invTwo = new Invoice('luigi', 'work on luigi website', 300)
+
+const invoices: Invoice[] = []
+invoices.push(invOne)
+invoices.push(invTwo)
+
+
+console.log(invoices);
