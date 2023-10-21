505f36dbbc96fabb82f2b58b8dc1f25ed5c95cd6
mahdimortuza <mahdi.webx@gmail.com>
2023-10-21 16:06:12 +0600

    module system

--- public/app.js
+++ public/app.js
@@ -1,19 +1,9 @@
-"use strict";
-class Invoice {
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from './classes/Invoice.js';
 const invOne = new Invoice('mario', 'work on mario website', 250);
 const invTwo = new Invoice('luigi', 'work on luigi website', 300);
 const invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format());
+    console.log(inv.client, inv.format());
 });
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,4 @@
-class Invoice {
-    constructor(
-        readonly client: string,
-        public details: string,
-        private amount : number,
-    ) { }
-
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`
-    }
-}
+import {Invoice} from './classes/Invoice.js'
 
 const invOne = new Invoice('mario', 'work on mario website', 250)
 const invTwo = new Invoice('luigi', 'work on luigi website', 300)
@@ -18,5 +8,5 @@ invoices.push(invOne)
 invoices.push(invTwo)
 
 invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format());
+    console.log(inv.client, inv.format());
 })
