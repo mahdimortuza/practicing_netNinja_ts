c993cb563884acf7e231266a0d2e5f4fe4b9f687
mahdimortuza <mahdi.webx@gmail.com>
2023-10-22 20:17:20 +0600

    do not understand generic

--- src/classes/Invoice.ts
+++ src/classes/Invoice.ts
@@ -1,13 +1,13 @@
-import { HasFormatter } from "../HasFormatter/HasFormatter"
+// import { HasFormatter } from "../HasFormatter/HasFormatter"
 
-export class Invoice implements HasFormatter {
-    constructor(
-        readonly client: string,
-        public details: string,
-        private amount: number,
-    ) { }
+// export class Invoice implements HasFormatter {
+//     constructor(
+//         readonly client: string,
+//         public details: string,
+//         private amount: number,
+//     ) { }
 
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`
-    }
-}
+//     format() {
+//         return `${this.client} owes $${this.amount} for ${this.details}`
+//     }
+// }
